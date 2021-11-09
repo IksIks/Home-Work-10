ccab4655c99588671e5a242424c6fb3acbdb86a3
iksiks30 <iksiks30@gmail.com>
2021-11-09 23:47:05 +0300

    создан класс User

---
+++ WPF_Telegram_Bot/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace WPF_Telegram_Bot
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public ObservableCollection<string> UserMessages { get; set; }
+
+        public User()
+        { }
+       public User(long id, string name, ObservableCollection<string> userMessages)
+        {
+            Id = id;
+            Name = name;
+            UserMessages = userMessages;
+        }
+
+
+    }
+}
